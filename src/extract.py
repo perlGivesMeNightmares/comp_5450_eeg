cf00100dfd6f432d8213d6ac75f5c237c6a87ef8
Emre Anamur <eanamur@gmail.com>
2019-03-23 14:20:32 -0400

    extraction script

---
+++ src/extract.py
@@ -0,0 +1,66 @@
+import os
+import numpy as np
+import pprint
+
+def parse_data():
+	basepath = path.dirname(__file__)
+	fp = path.abspath(path.join(basepath, "..", "data", "01", "1_raw_data_13-12_22.03.16.txt"))
+	return np.loadtxt(fp, delimiter="\t", skiprows=1)	
+
+def write_mat_data(data, folder_name, parent_filename, gesture):
+	new_filename = 'gesture_' + str(gesture) + '_user_' + folder_name + '_' + parent_filename
+	np.savetxt(new_filename, data, delimiter="\t", fmt="%s")
+	return True
+
+def filter_by_gesture(data, gesture):
+	gest_col = data[:, 9]
+
+	gesture_index_start = None
+	gesture_index_end = None
+	found_flag = False
+
+	lst = []
+
+	for idx, i in enumerate(gest_col):
+		if found_flag and i == gesture:
+			lst.append(data[idx])
+		elif found_flag and i != gesture:
+			gesture_index_end = idx
+			break
+		elif i == gesture:
+			gesture_index_start = idx
+			found_flag = True
+
+	return lst
+
+def main():
+	gesture_types = range(1, 8)
+
+	basepath = os.path.dirname(__file__)
+	fp = os.path.abspath(os.path.join(basepath, "..", "data"))
+	subdirectories = [name for name in os.listdir(fp) if os.path.isdir(os.path.join(fp, name))]
+	for dir_idx, directory in enumerate(subdirectories):
+		if dir_idx > 0:
+			return
+		sd_fp = os.path.abspath(os.path.join(basepath, "..", "data", str(directory)))
+		subfiles = os.listdir(sd_fp)
+		for file_name in subfiles:	
+			fp = os.path.abspath(os.path.join(basepath, "..", "data", directory, file_name))
+			txt_data = np.loadtxt(fp, delimiter="\t", skiprows=1)
+			gest_dict = {}
+			for gest in gesture_types:
+				result = filter_by_gesture(txt_data, gest)
+				if len(result) > 0:
+					success = write_mat_data(result, directory, file_name, gest)
+
+				
+
+	print(subdirectories)
+	return
+
+if __name__ == "__main__":
+	main()
+
+
+# we cant use the whole vector
+# plt.plot(time_col, channel_1)
--- src/parse.py
+++ src/parse.py
@@ -4,30 +4,52 @@ import scipy.signal as sig
 from os import path
 
 
-def parse_channels():
+def parse_data():
 	basepath = path.dirname(__file__)
 	fp = path.abspath(path.join(basepath, "..", "data", "01", "1_raw_data_13-12_22.03.16.txt"))
-	d = np.loadtxt(fp, delimiter="\t", skiprows=1)
+	return np.loadtxt(fp, delimiter="\t", skiprows=1)	
 
-	channels = {}
-	time_col = d[:, 0]
+def mat_to_columns(d):
+	column_dict = {}
+	column_dict['time'] = d[:, 0]
+	column_dict['gesture'] = d[:, 9]
 
 	for col_idx in range(1, 9):
-		channels['channel_'+str(col_idx)] = d[:, col_idx]
+		column_dict['channel_'+str(col_idx)] = d[:, col_idx]
 
-	return channels
+	return column_dict
 
-def make_graphs(data):
-	# teststft = spectrogram(data(:,1),hamming(256),50,512);
+def label_data(d, channel_num):
+	channel_col = d['channel_' + str(channel_num)]
+	label_col = d['gesture']
+	combined = np.column_stack((channel_col, label_col))
+	print('combined is %s' % len(combined))
+	return combined
+
+def make_stft(data):
 
 	Pxx, freqs, bins, im = plt.specgram(data['channel_1'], NFFT=256, Fs=1000, noverlap=128, cmap=plt.cm.gist_heat)
+	
 
-	# plt.plot(time_col, channel_1)
+def filter_by_gesture(data, gest_type):
+	"""get the first contiguous block of this gesture type"""
+	print(len(data))
+	return
+	filtered = data[data[:, 1] == gest_type]
+	channel_data = filtered[:, 0]
+	return channel_data
 
 
 def main():
-	data = parse_channels()
-	graphs = make_graphs(data)
+	CHANNEL = 1
+	GESTURE = 1
+
+	data = parse_data()
+	columns = mat_to_columns(data)
+	labeled = label_data(columns, CHANNEL)
+	gesture_data = filter_by_gesture(columns, GESTURE)
+	return
+	graphs = make_stft(gesture_data)
 	
 	plt.show()
 	return
@@ -37,3 +59,4 @@ if __name__ == "__main__":
 
 
 # we cant use the whole vector
+# plt.plot(time_col, channel_1)
