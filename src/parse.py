8a4e0939d2e2eb2a73f12ff983f825a0e4ca58cd
Emre Anamur <eanamur@gmail.com>
2019-03-23 12:09:14 -0400

    bugfix to parser

--- src/parse.py
+++ src/parse.py
@@ -13,20 +13,21 @@ def parse_channels():
 	time_col = d[:, 0]
 
 	for col_idx in range(1, 9):
-		channels['col_'+str(col_idx)] = d[:, col_idx]
+		channels['channel_'+str(col_idx)] = d[:, col_idx]
 
 	return channels
 
-def make_graphs():
+def make_graphs(data):
 	# teststft = spectrogram(data(:,1),hamming(256),50,512);
-	Pxx, freqs, bins, im = plt.specgram(channel_1, NFFT=200, Fs=500, noverlap=100, cmap=plt.cm.gist_heat)
+
+	Pxx, freqs, bins, im = plt.specgram(data['channel_1'], NFFT=256, Fs=1000, noverlap=128, cmap=plt.cm.gist_heat)
 
 	# plt.plot(time_col, channel_1)
 
 
 def main():
-	channels = parse_channels()
-	graphs = make_graphs()
+	data = parse_channels()
+	graphs = make_graphs(data)
 	
 	plt.show()
 	return
